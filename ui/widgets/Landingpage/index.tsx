aba443a9000fc29a95735e456eb8a352181c6efa
ginger <gingerlx56@gmail.com>
2025-10-15 13:10:04 +0100

    Added the Gamepage component and fixed related issues

--- ui/widgets/Landingpage/index.tsx
+++ ui/widgets/Landingpage/index.tsx
@@ -1,17 +1,20 @@
 // importing relevant module
 import React from "react";
 import { Typography } from "../../atoms/Typography";
+import useSound from 'use-sound';
 
 // import stylings
 import { LandingpageContainer, LandingpageComponent1, StartButton, LandingpageComponent2 } from "./index.styled";
 
 const Landingpage = (): JSX.Element => {
-
-    const setRedirect = () => {
-        // Add navigation logic here, e.g.:
-        // router.push("/dashboard") or window.location.href = "/dashboard"
-        console.log("Redirecting...");
-    };
+ const [play]:any= useSound('/assets/sound/enter_game.mp3');
+    const setRedirect:()=> void =  () => {
+        play()
+        setTimeout(() => {
+            window.location.pathname = '/games'
+        }, 1000)
+        
+    }
 
     return (
         <LandingpageContainer>
